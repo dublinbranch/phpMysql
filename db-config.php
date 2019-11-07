0abb7aad89386635ed947ed0f4f039a6d126bec8
Claudio Landi <claudio@tech.techadsmedia.com>
2019-11-07 18:59:10 +0000

    stable

---
+++ db-config.php
@@ -0,0 +1,13 @@
+<?php
+
+$db1 = new DBConf;
+$db1->db = "test";
+$db1->host = "127.0.0.1";
+$db1->passwd = "roy";
+$db1->user = "roy";
+
+$db2 = new DBConf;
+$db2->db = "test";
+$db2->host = "127.0.0.1";
+$db2->passwd = "claudio";
+$db2->user = "claudio";

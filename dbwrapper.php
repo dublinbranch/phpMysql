e09d20528000b1e41d5cb123cd2b78dd907a562f
Roy Bellingan <roy@tech.chameleon.ad>
2020-02-11 12:58:10 +0000

    deprecation of multiQuery

--- dbwrapper.php
+++ dbwrapper.php
@@ -29,46 +29,7 @@ class DBWrapper
 
     public function multiQuery(&$sql, $verbose = false, $keep = false)
     {
-        $start = microtime(1);
-        if ($verbose) {
-            echo "Executing $sql \n";
-        }
-        if (strlen($sql) < 2) {
-            $err = "$sql is too short SEPPUKU!\n";
-            fwrite(STDERR, $err);
-            file_put_contents("error.log", $err, FILE_APPEND | FILE_APPEND);
-            throw new Exception($err);
-        }
-        $res = $this->conn->multi_query($sql);
-
-        if ($res === false || $this->conn->error) {
-            $err = "$sql is wrong, error is " . $this->conn->error . "\n";
-            fwrite(STDERR, $err);
-            file_put_contents("error.log", $err, FILE_APPEND | FILE_APPEND);
-            throw new Exception($err);
-        }
-
-
-        $results = [];
-        do {
-            /* store first result set */
-            if ($result = $this->conn->store_result()) {
-                while ($row = $result->fetch_object()) {
-                    $results[] = $row;
-                }
-                $result->free();
-            }
-        } while ($this->conn->next_result());
-
-        $time = microtime(1) - $start;
-        if (defined('VERBOSE_SQL_TIME') && VERBOSE_SQL_TIME == true) {
-            echo "\n ---------- \n$sql\nin $time \n";
-        }
-        if (!$keep) {
-            $sql = '';
-            unset($sql);
-        }
-        return $results;
+       die("is in some way broken, I (Roy) am not able to find a single working example with proper error reporting");
     }
 
     public function singleShotQuery($sql, $verbose = false, $keep = false)

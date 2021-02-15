a726b66c5b5521a6bfb7bd85bb4dcf8617e5762f
Roy Bellingan <roy@tech.techadsmedia.com>
2021-02-15 18:43:06 +0000

    ssl optional

--- dbwrapper.php
+++ dbwrapper.php
@@ -47,15 +47,16 @@ if (!function_exists("dummyDbWrapper")) {
                 die('mysqli_init failed');
             }
 
-            if (!$mysqli->options(MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 0')) {
-                die('Setting MYSQLI_INIT_COMMAND failed');
-            }
-
             if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
                 die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
             }
 
-            if (!$mysqli->real_connect($conf->host, $conf->user, $conf->passwd, $conf->db, $conf->port, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT | MYSQLI_CLIENT_SSL)) {
+            $flag = 0;
+            if($conf->ssl){
+                $flag |= MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT | MYSQLI_CLIENT_SSL;
+            }
+
+            if (!$mysqli->real_connect($conf->host, $conf->user, $conf->passwd, $conf->db, $conf->port, NULL, $flag)) {
                 die('Connect Error (' . mysqli_connect_errno() . ') '
                     . mysqli_connect_error());
             }
